a6f417aa17fb0af0ac39f3c33311041d8b1e7355
Thara <91600181+ktea12@users.noreply.github.com>
2022-01-10 20:05:27 +0700

    major change 1

---
+++ driver mood tracker.py
@@ -0,0 +1,32 @@
+from moodtracker import moodcalc
+
+def mainmenu():
+    confirm = str(input("Welcome to Daily Mood Tracker \n Do you wish to evaluate your mood today? (YES/NO) "))
+    while True: #keeps the user to prompt the question and answer with "YES" or "NO"
+        if confirm.upper == "YES":
+            pass
+        elif confirm.upper == "NO":
+            break
+        else:
+            print("Please input YES or NO")
+
+    def moodlistQL():
+        moodlist = [] #creating an empty list
+
+        while True: #keeps the user to prompt atleast more than 0
+            print("Please input a number from 1 to 10")
+            morning = int(input("At what level was your mood in the morning? ")) 
+            noon = int(input("At what level was your mood at noon? ")) #here is to input the mood level using int which accepts integers only
+            night = int(input("At what level was your mood at night? "))
+            moodlvl = moodlistQL(morning, noon, night)
+            if 11 > morning == 0:
+                print("You must input a number above 0 and less than 10") #check if input level is 0 or above 10
+            elif 11 > noon == 0:
+                print("You must input a number above 0 and less than 10")
+            elif 11 > night == 0:
+                print("You must input a number above 0 and less than 10")
+            else:
+                break
+        moodlist.append(moodlvl) #adds object to the mood list created earlier
+    
+    return moodlist
---
+++ mood tracker.py
@@ -0,0 +1,19 @@
+class moodcalc: #create class for calculating moods
+
+    import math #import math to be used in calculations 
+
+    def __init__(self, morning, noon, night): #getter function, getting values: time of day, and level of mood
+        self.tmorn = morning #level of mood in the morning
+        self.tnoon = noon #level of mood at noon
+        self.tnight = night #level of mood at night
+
+    def get_avg(self):
+        self.tmorn()
+        self.tnoon()
+        self.tnight()
+        self.total = (self.tmorn * self.tnoon * self.tnight)//3
+        return self.total
+
+    def __str__(self) -> str:
+        self.get_avg()
+        return (f"Your mood averaged at level: {self.total}")
