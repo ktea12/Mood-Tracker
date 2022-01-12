7255d204ee447c57032f1025c565b6cd63a5274a
Thara <91600181+ktea12@users.noreply.github.com>
2022-01-12 15:14:49 +0700

    tkinter change

--- moodtracker.py
+++ moodtracker.py
@@ -20,6 +20,5 @@ class moodcalc: #create class for calculating moods
 
 class graphmood(moodcalc): #create class for graphing moods
     import matplotlib.pyplot as plt
-
-plt.plot(range(10), moodlist)
-plt.show()
+    plt.plot(range(10), moodlist)
+    plt.show()
--- tkinter mood.py
+++ tkinter mood.py
@@ -1,5 +1,20 @@
 import tkinter as tk
 
+HEIGHT = 700
+WIDTH = 1400
+
 root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=HEIGHT) #background
+canvas.pack()
+
+label = tk.Label(text="Welcome to Daily Mood Tracker \n Do you wish to evaluate your mood today? (YES/NO)")
+label.pack()
+
+entry = tk.Entry(font=40)
+entry.pack()
+
+button = tk.Button(root, text="Next")
+button.pack()
 
 root.mainloop()
