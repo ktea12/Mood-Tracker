08f64f97c500afd458d6d43e18813098bca44ff8
Thara <91600181+ktea12@users.noreply.github.com>
2022-01-16 00:19:29 +0700

    Update tkinter mood input.py

--- tkinter mood input.py
+++ tkinter mood input.py
@@ -7,10 +7,10 @@ root.geometry("1000x500")
 WIDTH = 20
 HEIGHT = 10
 
-label =tkinter.Label (font=20, text="Welcome to Mood track.")
-label.grid(row=1, column=4, pady=50)
-label =tkinter.Label (font=20, text="What Level is your Mood at?")
-label.grid(row=1, column=5, pady=50)
+label =tkinter.Label (text="Welcome to Mood track.")
+label.grid(row=1, column=4, pady=20)
+label =tkinter.Label (text="What Level is your Mood at?")
+label.grid(row=1, column=5, pady=20)
 
 button0=tkinter.Button(root, text="0\n\nVery Bad", width=WIDTH, height=HEIGHT)
 button0.grid(row=2, column=2, padx=7, pady=10)
@@ -30,9 +30,9 @@ button4.grid(row=2,column=6, padx=7, pady=10)
 button5=tkinter.Button(root, text="5\n\nVery Good Mood", width=WIDTH, height=HEIGHT)
 button5.grid(row=2,column=7, padx=7, pady=10)
 
-back =tkinter.Button ( text="Go to Main Page")
-back.grid(row=3, column=4, pady=10)
-graph =tkinter.Button ( text="Plot Graph")
-graph.grid(row=3, column=5, pady=10)
+back =tkinter.Button (text="Go to Main Page")
+back.grid(row=3, column=4, pady=20)
+graph =tkinter.Button (text="Plot Graph")
+graph.grid(row=3, column=5, pady=20)
 
 root.mainloop()
