4f9042ec52637c7e5c80aa68261b1c434088fae4
Thara <91600181+ktea12@users.noreply.github.com>
2022-01-15 14:08:53 +0700

    mood

--- tkinter mood.py
+++ tkinter mood.py
@@ -1,5 +1,16 @@
 import tkinter as tk
 
+def mainmenu():
+    confirm = entry.get().strip().upper()
+    while True: #keeps the user to prompt the question and answer with "YES" or "NO"
+        if confirm == "YES":
+            pass
+        elif confirm == "NO":
+            label['text'] = "Goodbye!\nInput YES to evaluate Mood."
+
+        else:
+            label['text'] = "Please input YES or NO\nDo you wish to evaluate your Mood today?"
+
 HEIGHT = 500
 WIDTH = 700
 
@@ -8,13 +19,18 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=HEIGHT) #background
 canvas.pack()
 
-label = tk.Label(font=50, text="Welcome to Daily Mood Tracker \n Do you wish to evaluate your mood today? (YES/NO)")
+label = tk.Label(font=50, text="Welcome to Daily Mood Tracker \nDo you wish to evaluate your Mood today? (YES/NO)")
 label.place(relwidth=1, relheight=0.5)
 
 entry = tk.Entry(font=40)
 entry.place(relx=0.35, rely=0.4, relwidth=0.3)
 
-button = tk.Button(root,font=50, text="Next")
+entry.bind('<Return>', mainmenu) 
+
+button = tk.Button(root,font=50, text="Enter", command=mainmenu)
 button.place(relx=0.35, rely=0.5, relwidth=0.3, relheight=0.07)
 
-root.mainloop()
+
+entry.focus() 
+
+root.mainloop() #for window display
