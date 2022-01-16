94a4eec66c7139ae1d4c44640c81e0b180be2fba
Thara <91600181+ktea12@users.noreply.github.com>
2022-01-16 15:19:55 +0700

    Update tkinter mood input.py

--- tkinter mood input.py
+++ tkinter mood input.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import tkinter
 
 root=tkinter.Tk()
@@ -50,4 +51,33 @@ back.grid(row=6, column=4, pady=20)
 graph =tkinter.Button (text="Plot Graph")
 graph.grid(row=6, column=5, pady=20)
 
+class moodcalc: #create class for calculating moods
+
+    def __init__(self, morning, noon, night): #getter function, getting values: time of day, and level of mood
+        self.tmorn = morning #level of mood in the morning
+        self.tnoon = noon #level of mood at noon
+        self.tnight = night #level of mood at night
+        self.moodscore = [self.tmorn, self.tnoon, self.tnight]
+        self.time_of_day = ['Morning', 'Noon', 'Night']
+
+    def get_avg(self):
+        self.tmorn()
+        self.tnoon()
+        self.tnight()
+        self.total = (self.tmorn + self.tnoon + self.tnight)//3
+        return self.total
+
+    def __str__(self) -> str: #setter function
+        self.get_avg()
+        return (f"Your mood averaged at level: {self.total}")
+
+    def graph_mood_score(self):
+        plt.plot(self.time_of_day, self.moodscore)
+        plt.xlabel('Time of Day')
+        plt.ylabel('Mood Level')
+        plt.title('Mood Level over Time')
+        plt.show()
+
+mood=moodcalc(3,5,2)
+mood.graph_mood_score()
 root.mainloop()
